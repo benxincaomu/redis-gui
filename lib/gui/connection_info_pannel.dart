d339942aabb4eccc53c223270de172aab610a76e
benxincaomu <benxincaomu@foxmail.com>
2023-05-02 18:17:49 +0800

    新建连接表单UI

--- lib/gui/connection_info_pannel.dart
+++ lib/gui/connection_info_pannel.dart
@@ -1,22 +1,76 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
-
-class ConnectionInfoPannel extends StatelessWidget{
+class ConnectionInfoPannel extends StatefulWidget {
   const ConnectionInfoPannel({super.key});
-  
+
+  void save() {}
+  void cancel() {}
+
+  @override
+  State<StatefulWidget> createState() {
+    return ConnectionInfoPannelState();
+  }
+}
+
+class ConnectionInfoPannelState extends State<ConnectionInfoPannel> {
+  final _formKey = GlobalKey<FormState>();
+  final nameController = TextEditingController();
+  final serverAddressController = TextEditingController();
+  final serverPortController = TextEditingController();
+
+  final userNameController = TextEditingController();
+
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    
-    //return const Scaffold();
-
-    return const Text("Connection Info Pannel");
+    return Form(
+        key: _formKey,
+        autovalidateMode: AutovalidateMode.always,
+        child: Column(children: [
+          TextFormField(
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: "名称",
+            ),
+            controller: nameController,
+          ),
+          TextFormField(
+              decoration: const InputDecoration(
+            border: UnderlineInputBorder(),
+            labelText: "服务地址",
+          ),
+          controller: serverAddressController),
+          TextFormField(
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: "端口",
+              ),
+              controller: serverPortController,
+              keyboardType: TextInputType.number,
+              validator: (value) {
+                if (value == null ||
+                    value.isEmpty ||
+                    int.tryParse(value) != null) {
+                  return null;
+                }
+                return "端口号必须是整数";
+              }),
+          TextFormField(
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: "用户名",
+            ),
+            controller: userNameController,
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: "密码",
+            ),
+            controller: passwordController,
+            obscureText: true,
+          ),
+        ]));
   }
-
-  void save(){
-
-  }
-  void cancel(){
-
-  }
-
 }
--- lib/gui/menubar_gui.dart
+++ lib/gui/menubar_gui.dart
@@ -14,24 +14,27 @@ class MenuBarGui extends StatelessWidget {
           children: <Widget>[
             SubmenuButton(menuChildren: <Widget>[
               MenuItemButton(
-                  child: const MenuAcceleratorLabel("New Connection"),
+                  child: const MenuAcceleratorLabel("新连接"),
                   onPressed: () {
                     //showAboutDialog(context: context, applicationName: "New Connection");
-                    showDialog(context: context, builder: (context)=>const AlertDialog(
-                      title: Text("New Connection"),
-                      content: ConnectionInfoPannel(),
-                      actions: [
-
-                      ],
-                    ));
+                    showDialog(
+                        context: context,
+                        builder: (context) => const AlertDialog(
+                            title: Text("新连接"),
+                            content: SizedBox(
+                              width: 500,
+                              height: 500,
+                              child: ConnectionInfoPannel(),
+                            )));
                   }),
               MenuItemButton(
-                  child: const MenuAcceleratorLabel("Quit"), onPressed: () {})
-            ], child: const MenuAcceleratorLabel("File")),
+                  child: const MenuAcceleratorLabel("退出"), onPressed: () {})
+            ], child: const MenuAcceleratorLabel("文件")),
             SubmenuButton(menuChildren: <Widget>[
               MenuItemButton(
-                  child: const MenuAcceleratorLabel("Quick Connect"), onPressed: () {})
-            ], child: const MenuAcceleratorLabel("Connections"))
+                  child: const MenuAcceleratorLabel("快速连接"),
+                  onPressed: () {})
+            ], child: const MenuAcceleratorLabel("连接"))
           ],
         ))
       ],
